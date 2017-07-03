7c4700854293e1bd22cd766b1c206eccbf051c40
newtonkiragu <karanunewton4@gmail.com>
2017-07-03 09:31:20 +0300

    made the app be able to count instances of words other than the word that is being searched for

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,8 +2,10 @@ class String
   define_method(:count) do |word_to_count|
     counter = 0
     split = self.split()
+    extra = word_to_count
     split.each() do |word|
-      if word==word_to_count
+      word_to_count = word.gsub(/[!,@,?,"",:,;,.]/, "")
+      if word.include? extra
         counter+=1
       end
     end
